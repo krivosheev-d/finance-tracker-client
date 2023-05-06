6af01549736f96454b597ce5eb0f83db4d071d18
spokik <tackesi@yandex.ru>
2023-05-06 15:58:19 +0300

    Add pages

---
+++ src/app/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import NavBar from '~/shared/components/NavBar.vue';
+</script>
+
+<template>
+  <nav-bar />
+
+  <router-view />
+</template>
+
--- src/app/router/routes.ts
+++ src/app/router/routes.ts
@@ -2,12 +2,37 @@ const routes = [
   {
     path: '/',
     name: 'homePage',
-    component: () => import('~/shared/components/Home.vue'),
+    component: () => import('~/pages/HomePage.vue'),
+  },
+  {
+    path: '/registration',
+    name: 'registration',
+    component: () => import('~/pages/RegistrationPage.vue'),
+  },
+  {
+    path: '/authorization',
+    name: 'authorization',
+    component: () => import('~/pages/AuthorizationPage.vue'),
   },
   {
     path: '/second',
     name: 'secondPage',
-    component: () => import('~/shared/components/SecondPage.vue'),
+    component: () => import('~/pages/SecondPage.vue'),
+  },
+  {
+    path: '/statistics',
+    name: 'statistics',
+    component: () => import('~/pages/StatisticsPage.vue'),
+  },
+  {
+    path: '/management',
+    name: 'management',
+    component: () => import('~/pages/FinManagementPage.vue'),
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: () => import('~/pages/ProfilePage.vue'),
   },
 ];
 
