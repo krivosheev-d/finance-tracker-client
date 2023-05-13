247763e95e13528cf3c134f0f43cb02e5f902d26
spokik <tackesi@yandex.ru>
2023-05-14 00:30:25 +0300

    add BaseButton

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -6,39 +6,71 @@ import BaseButton from '~/shared/ui/BaseButton.vue';
 	<div>
 		Home
 		<div class="dark">
-			<base-button icon="add" text="Okay" />
+			<BaseButton text>
+				sdfds
+			</BaseButton>
 
-			<base-button icon="add" text="Okay" outlined />
+			<base-button></base-button>
+			<BaseButton label="Link" link />
 
-			<base-button icon="done" text="Approve" variant="success" />
+			<BaseButton label="Secondary" severity="secondary" rounded />
 
-			<base-button icon="done" text="Approve" variant="success" outlined />
+			<BaseButton icon="pi pi-check" aria-label="Submit" />
+			<BaseButton label="Submit" icon="pi pi-check" />
+			<BaseButton label="Submit" icon="pi pi-check" iconPos="right" />
 
-			<base-button icon="close" text="Dismiss" variant="error" />
+			<BaseButton label="Primary" />
+			<BaseButton label="Secondary" severity="secondary" />
+			<BaseButton label="Success" severity="success" />
+			<BaseButton label="Info" severity="info" />
+			<BaseButton label="Warning" severity="warning" />
+			<BaseButton label="Help" severity="help" />
+			<BaseButton label="Danger" severity="danger" />
 
-			<base-button icon="close" text="Dismiss" variant="error" outlined />
 
-			<base-button icon="home" text="Go home" variant="primary" />
+			<BaseButton icon="pi pi-check" aria-label="Filter" />
+			<BaseButton icon="pi pi-bookmark" severity="secondary" aria-label="Bookmark" />
+			<BaseButton icon="pi pi-search" severity="success" aria-label="Search" />
+			<BaseButton icon="pi pi-user" severity="info" aria-label="User" />
+			<BaseButton icon="pi pi-bell" severity="warning" aria-label="Notification" />
+			<BaseButton icon="pi pi-heart" severity="help" aria-label="Favorite" />
+			<BaseButton icon="pi pi-times" severity="danger" aria-label="Cancel" />
 
-			<base-button icon="home" text="Go home" variant="primary" outlined />
+			<BaseButton icon="pi pi-check" rounded aria-label="Filter" />
+			<BaseButton icon="pi pi-bookmark" severity="secondary" rounded aria-label="Bookmark" />
+			<BaseButton icon="pi pi-search" severity="success" rounded aria-label="Search" />
+			<BaseButton icon="pi pi-user" severity="info" rounded aria-label="User" />
+			<BaseButton icon="pi pi-bell" severity="warning" rounded aria-label="Notification" />
+			<BaseButton icon="pi pi-heart" severity="help" rounded aria-label="Favorite" />
+			<BaseButton icon="pi pi-times" severity="danger" rounded aria-label="Cancel" />
+
+			<BaseButton icon="pi pi-check" rounded outlined aria-label="Filter" />
+			<BaseButton icon="pi pi-bookmark" severity="secondary" rounded outlined aria-label="Bookmark" />
+			<BaseButton icon="pi pi-search" severity="success" rounded outlined aria-label="Search" />
+			<BaseButton icon="pi pi-user" severity="info" rounded outlined aria-label="User" />
+			<BaseButton icon="pi pi-bell" severity="warning" rounded outlined aria-label="Notification" />
+			<BaseButton icon="pi pi-heart" severity="help" rounded outlined aria-label="Favorite" />
+			<BaseButton icon="pi pi-times" severity="danger" rounded outlined aria-label="Cancel" />
+
+			<BaseButton icon="pi pi-check" text raised rounded aria-label="Filter" />
+			<BaseButton icon="pi pi-bookmark" severity="secondary" text raised rounded aria-label="Bookmark" />
+			<BaseButton icon="pi pi-search" severity="success" text raised rounded aria-label="Search" />
+			<BaseButton icon="pi pi-user" severity="info" text raised rounded aria-label="User" />
+			<BaseButton icon="pi pi-bell" severity="warning" text raised rounded aria-label="Notification" />
+			<BaseButton icon="pi pi-heart" severity="help" text raised rounded aria-label="Favorite" />
+			<BaseButton icon="pi pi-times" severity="danger" text raised rounded aria-label="Cancel" />
+
+			<BaseButton icon="pi pi-check" text rounded aria-label="Filter" />
+			<BaseButton icon="pi pi-bookmark" severity="secondary" text rounded aria-label="Bookmark" />
+			<BaseButton icon="pi pi-search" severity="success" text rounded aria-label="Search" />
+			<BaseButton icon="pi pi-user" severity="info" text rounded aria-label="User" />
+			<BaseButton icon="pi pi-bell" severity="warning" text rounded aria-label="Notification" />
+			<BaseButton icon="pi pi-heart" severity="help" text rounded aria-label="Favorite" />
+			<BaseButton icon="pi pi-times" severity="danger" text rounded aria-label="Cancel" />
+
+
 		</div>
-		<div class="light">
-			<base-button icon="add" text="Okay" />
 
-			<base-button icon="add" text="Okay" outlined />
-
-			<base-button icon="done" text="Approve" variant="success" />
-
-			<base-button icon="done" text="Approve" variant="success" outlined />
-
-			<base-button icon="close" text="Dismiss" variant="error" />
-
-			<base-button icon="close" text="Dismiss" variant="error" outlined />
-
-			<base-button icon="home" text="Go home" variant="primary" />
-
-			<base-button icon="home" text="Go home" variant="primary" outlined />
-		</div>
 	</div>
 </template>
 
--- src/shared/ui/BaseButton.vue
+++ src/shared/ui/BaseButton.vue
@@ -1,210 +1,22 @@
+<script lang="ts" setup>
+import Button from 'primevue/button';
+import type { ButtonProps } from 'primevue/button/Button';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface ButtonPropsIgnore extends /* @vue-ignore */ ButtonProps { }
+
+defineProps<ButtonPropsIgnore>();
+
+</script>
+
 <template>
-  <button
-    :class="[
-      'base-button',
-      props.outlined ? `${getVariantClass()}_outlined` : getVariantClass()
-    ]"
-  >
-    <span
-      v-if="props.icon && props.iconParams.position === 'left'"
-      class="material-symbols-outlined"
-    >
-      {{ props.icon }}
-    </span>
+  <Button :label="label" :icon="icon" :iconPos="iconPos" :iconClass="iconClass" :badge="badge" :badgeClass="badgeClass"
+    :loading="loading" :loadingIcon="loadingIcon" :link="link" :severity="severity" :raised="raised" :rounded="rounded"
+    :text="text" :outlined="outlined" :size="size" :plain="plain">
+    <slot>
 
-    <span class="base-button__text button-text">
-      <slot>
-        {{ props.text }}
-      </slot>
-    </span>
-
-
-    <span
-      v-if="props.icon && props.iconParams.position === 'right'"
-      class="material-symbols-outlined"
-    >
-      {{ props.icon }}
-    </span>
-  </button>
+    </slot>
+  </Button>
 </template>
 
-<script lang="ts" setup>
-type TIconParams = {
-  position?: 'left' | 'right',
-}
-
-interface IProps {
-    outlined?: boolean,
-    icon?: string,
-    iconParams?: TIconParams,
-    text?: string,
-    variant?: 'default' | 'primary' | 'success' | 'error',
-}
-
-const props = withDefaults(defineProps<IProps>(), {
-  outlined: false,
-  iconParams: ():TIconParams => ({
-    position: 'left',
-  }),
-  variant: 'default',
-});
-
-const getVariantClass = () => `base-button_${props.variant}`;
-</script>
-
-<script lang="ts">
-export default {
-  name: 'BaseButton'
-};
-</script>
-
-<style lang="scss" scoped>
-@import "@/app/styles/colors";
-
-.base-button {
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 4px;
-  padding: 6px 24px;
-  width: fit-content;
-  border-radius: 4px;
-  border-width: 2px;
-  border-style: solid;
-  cursor: pointer;
-
-  &__text {
-    align-self: center;
-  }
-
-  &:before {
-    position: absolute;
-    content: '';
-    left: -2px;
-    top: -2px;
-    right: -2px;
-    bottom: -2px;
-    opacity: 0;
-    border-radius: 4px;
-    transition: 0.2s all;
-  }
-  &:hover {
-    &:before {
-      opacity: 0.1;
-    }
-  }
-}
-
-.dark {
-  .base-button {
-    &:before {
-      background-color: #FFFFFF;
-    }
-
-    &_default {
-      background-color: $btn-neutral-text;
-      border-color: $btn-neutral-text;
-      color: $btn-neutral;
-
-      &_outlined {
-        background-color: transparent;
-        border-color: $btn-neutral;
-        color: $btn-neutral;
-      }
-    }
-
-    &_primary {
-      background-color: $btn-primary;
-      border-color: $btn-primary;
-      color: $btn-text-default;
-
-      &_outlined {
-        background-color: transparent;
-        border-color: $btn-primary;
-        color: $btn-primary-enlightened;
-      }
-    }
-
-    &_success {
-      background-color: $btn-green;
-      border-color: $btn-green;
-      color: $btn-text-default;
-
-      &_outlined {
-        background-color: transparent;
-        border-color: $btn-green;
-        color: $btn-green;
-      }
-    }
-
-    &_error {
-      background-color: $btn-red;
-      border-color: $btn-red;
-      color: $btn-text-default;
-
-      &_outlined {
-        background-color: transparent;
-        border-color: $btn-red;
-        color: $btn-red;
-      }
-    }
-  }
-}
-
-.light {
-  .base-button {
-    &:before {
-      background-color: #000000;
-    }
-
-    &_default {
-      background-color: $btn-neutral;
-      border-color: $btn-neutral;
-      color: $btn-neutral-text;
-
-      &_outlined {
-        background-color: transparent;
-        border-color: $btn-neutral-text;
-        color: $btn-neutral-text;
-      }
-    }
-
-    &_primary {
-      background-color: $btn-primary;
-      border-color: $btn-primary;
-      color: $btn-text-default;
-
-      &_outlined {
-        background-color: transparent;
-        border-color: $btn-primary;
-        color: $btn-primary;
-      }
-    }
-
-    &_success {
-      background-color: $btn-green;
-      border-color: $btn-green;
-      color: $btn-text-default;
-
-      &_outlined {
-        background-color: transparent;
-        border-color: $btn-green;
-        color: $btn-green;
-      }
-    }
-
-    &_error {
-      background-color: $btn-red;
-      border-color: $btn-red;
-      color: $btn-text-default;
-
-      &_outlined {
-        background-color: transparent;
-        border-color: $btn-red;
-        color: $btn-red;
-      }
-    }
-  }
-}
-</style>
+<style lang="scss" scoped></style>
