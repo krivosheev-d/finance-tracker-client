88b14104cd7bcd5654de1459cd05c366149fd8a6
spokik <tackesi@yandex.ru>
2023-05-16 00:32:33 +0300

    add InputMask

--- src/pages/AuthorizationPage.vue
+++ src/pages/AuthorizationPage.vue
@@ -1,9 +1,32 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
+import InputMask from '~/shared/ui/InputMask.vue';
+
+const textValue = ref('dsafsd');
+
+function ooo() {
+  alert('ooo');
+}
+
 </script>
 
 <template>
   <div>
-    Authorization
+    <a href="https://primevue.org/inputmask/" target="_blank" rel="noopener noreferrer">inputmask</a>
+    <div>
+      <span>tel</span><br>
+      <InputMask mask="+7(999)999-99-99" placeholder="+7(999)999-99-99" />
+    </div>
+    <div>
+      <span>email</span><br>{{ textValue }} <br>
+      <InputMask mask="+7(999)999-99-99" v-model="textValue" placeholder="+7(999)999-99-99" />
+    </div>
+    <div>
+      <span>password</span><br>
+      <InputMask mask="+7(999)999-99-99" placeholder="+7(999)999-99-99" />
+    </div>
+
+
   </div>
 </template>
 
