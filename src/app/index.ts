85f506c17af7972be335579d977d21569a84452e
spokik <61501832+spokik@users.noreply.github.com>
2023-05-15 13:39:02 +0300

    add base button (#1)
    * test
    
    * add BaseButton

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,9 +2,9 @@ import app from './app';
 import router from './router';
 import store from './store';
 import http from './http';
+import PrimeVue from 'primevue/config';
 
-app.use(store);
-app.use(router);
+app.use(store).use(router).use(PrimeVue);
 router.isReady().then(() => {
   app.mount('#app');
 });
--- src/shared/components/NavBar.vue
+++ src/shared/components/NavBar.vue
@@ -1,39 +1,53 @@
-<template>
-  <nav class="navbar">
-    <router-link
-      :to="{
-        name: 'homePage'
-      }"
-      class="navbar__link"
-    >
-      Home page
-    </router-link>
-
-    <router-link
-      :to="{
-        name: 'secondPage'
-      }"
-      class="navbar__link"
-    >
-      Second page
-    </router-link>
-  </nav>
-  {{ isLoggedIn ? 'Logged as' : 'Not logged' }}
-  {{ isLoggedIn ? `: ${profile.name}` : '' }}
-</template>
 
 <script lang="ts" setup>
 import { useAuthStore } from '~/app/store/auth';
 
 const authStore = useAuthStore();
-const isLoggedIn = authStore.isLoggedIn;
-const profile = authStore.getProfile;
+const isLoggedIn = authStore.isLoggedIn ? 'Logged as' : 'Not logged';
+const profileNmae = authStore.getProfile ? `: ${authStore.getProfile.name}` : '';
 </script>
+
+<template>
+  <nav class="navbar">
+    <router-link :to="{ name: 'homePage' }" class="navbar__link">
+      Home page
+    </router-link>
+    |
+    <router-link :to="{ name: 'secondPage' }" class="navbar__link">
+      Second page
+    </router-link>
+    |
+    <router-link :to="{ name: 'registration' }" class="navbar__link">
+      registration
+    </router-link>
+    |
+    <router-link :to="{ name: 'authorization' }" class="navbar__link">
+      authorization
+    </router-link>
+    |
+    <router-link :to="{ name: 'statistics' }" class="navbar__link">
+      statistics
+    </router-link>
+    |
+    <router-link :to="{ name: 'management' }" class="navbar__link">
+      management
+    </router-link>
+    |
+    <router-link :to="{ name: 'profile' }" class="navbar__link">
+      profile
+    </router-link>
+    |
+
+  </nav>
+  {{ isLoggedIn }}
+  {{ profileNmae }}
+</template>
+
 
 <style lang="scss" scoped>
 .navbar {
   &__link {
-    color: red!important;
+    color: red !important;
   }
 }
 </style>
